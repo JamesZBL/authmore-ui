d22c4d1bc4058c5d4976ab0cc3cc91bc13a73474
摆码王子 <1146556298@qq.com>
2019-04-19 16:48:40 +0800

    update title

--- src/components/SiderMenu/SiderMenu.js
+++ src/components/SiderMenu/SiderMenu.js
@@ -79,7 +79,7 @@ export default class SiderMenu extends PureComponent {
         <div className={styles.logo} id="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
-            <h1>Ant Design Pro</h1>
+            <h1>Authmore</h1>
           </Link>
         </div>
         <Suspense fallback={<PageLoading />}>
